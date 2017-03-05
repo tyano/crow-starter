c58b97a5fe806090cc8e32f0ebe27732fb6d1d54
Tsutomu YANO <t_yano@me.com>
2017-03-05 14:03:47 +0900

    changes version to 2.0 / uses `configuration/from` for loading configuration files

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject crow-starter "1.0-SNAPSHOT"
+(defproject crow-starter "2.0-SNAPSHOT"
   :description "a utility library for starting crow-server."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:provided {:dependencies [[crow "1.0-SNAPSHOT" :scope "provided"]]}}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]]
+  :profiles {:provided {:dependencies [[crow "2.0-SNAPSHOT" :scope "provided"]]}}
   :repositories {"javelindev-snapshots" "http://javelindev.jp/repository/snapshots"}
   :aot [crow.starter.core]
   :main crow.starter.core)
--- src/crow/starter.clj
+++ src/crow/starter.clj
@@ -5,11 +5,12 @@
 ;;; this library.
 (ns crow.starter
   (:require [crow.protocol :refer [install-default-marshaller]]
-            [crow.configuration :as config]))
+            [crow.configuration :as config]
+            [async-connect.server :refer [close-wait]]))
 
 (defn- load-starter-fn
   [conf]
-  (if-let [starter-fn-name (:starter conf)]
+  (if-let [starter-fn-name (:service/starter conf)]
     (let [fn-symbol (if (symbol? starter-fn-name) starter-fn-name (symbol starter-fn-name))
           fn-ns     (namespace fn-symbol)]
       (require (symbol fn-ns))
@@ -17,21 +18,13 @@
       (if-let [starter-fn (find-var fn-symbol)]
         starter-fn
         (throw (IllegalStateException. (str "No such function: " starter-fn-name)))))
-    (throw (IllegalStateException. ":starter key is not found in your configuration file."))))
+    (throw (IllegalStateException. ":service/starter key is not found in your configuration file."))))
 
 (defn launch
   [config-path]
-  (let [conf       (if (.endsWith config-path ".clj")
-                      (config/from-path config-path)
-                      (config/from-edn config-path))
-        marshaller (or (:object-marshaller conf)
-                       (throw (IllegalStateException.
-                                "Couldn't get an instance of object-marshaller. Mayby no :object-marshaller in config file.")))]
-    (install-default-marshaller marshaller)
-    (let [starter-fn (load-starter-fn conf)]
-      (println (str "SERVICE STARTS: " (:name conf)))
-      (let [server (starter-fn conf)]
-        (.. (Runtime/getRuntime) (addShutdownHook (Thread. (fn [] (.close server) (println "SERVER STOPPED.")))))
-        (while true
-          (Thread/sleep 1000))))))
+  (let [conf       (config/from config-path)
+        starter-fn (load-starter-fn conf)]
+    (println (str "SERVICE STARTS: " (:service/name conf)))
+    (let [server (starter-fn conf)]
+      (close-wait server #(println "SERVER STOPPED.")))))
 
