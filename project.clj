9e778039e76631d50d4efcbb2daba3d44f70c573
Tsutomu YANO <t_yano@me.com>
2018-02-04 20:23:14 +0900

    Cloure 1.9.0

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]]
   :profiles {:provided {:dependencies [[crow "2.0-SNAPSHOT" :scope "provided"]]}}
   :repositories {"javelindev-snapshots" "http://javelindev.jp/repository/snapshots"}
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :aot [crow.starter.core]
   :main crow.starter.core)
 
