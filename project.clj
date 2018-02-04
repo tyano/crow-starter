9dafd9b0b44ad6561ee8bc4c4454eeffd8220d0f
Tsutomu YANO <t_yano@me.com>
2018-02-04 20:25:13 +0900

    added :deploy-repositories

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories {"javelindev-snapshots" "http://javelindev.jp/repository/snapshots"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
   :profiles {:provided {:dependencies [[crow "2.1-SNAPSHOT" :scope "provided"]]}}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
   :aot [crow.starter.core]
   :main crow.starter.core)
